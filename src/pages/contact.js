af712f033e50e12629b77ae3b23814d650de441a
Rouncer27 <trevor@switchbackcreative.ca>
2021-06-23 15:44:27 -0600

    init commit

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const contact = () => {
+  return (
+    <Layout>
+      <h1>Hello Contact Page!!!</h1>
+    </Layout>
+  )
+}
+
+export default contact
